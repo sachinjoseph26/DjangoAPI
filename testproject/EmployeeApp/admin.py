f39aa09d1d8c075fc610d485ae70c24354466831
sachinpjaj26 <sachinjoseph054@gmail.com>
2024-01-27 01:03:48 -0500

    Final Commit

--- testproject/EmployeeApp/admin.py
+++ testproject/EmployeeApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from EmployeeApp.models import Employees,Departments
 # Register your models here.
+
+admin.site.register(Departments)
+admin.site.register(Employees)
